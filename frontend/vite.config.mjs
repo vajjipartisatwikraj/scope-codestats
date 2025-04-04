6afad9d794e7923208ec61e6229aab2c56c92243
vajjipartisatwikraj <satwikrajv@gmail.com>
2025-04-04 21:30:56 +0530

    Update vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://codestats.zapto.org',
         changeOrigin: true,
         secure: false
       }
