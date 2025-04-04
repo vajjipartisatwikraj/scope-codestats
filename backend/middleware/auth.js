53f58012fefd0eb9e6edf21562647f06deab1e28
vajjipartisatwikraj <satwikrajv@gmail.com>
2025-04-04 22:49:09 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,5 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
