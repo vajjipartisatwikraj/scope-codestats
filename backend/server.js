3b91e92034b80100a1c85abb4f9ddfc9d25fc8b1
vajjipartisatwikraj <satwikrajv@gmail.com>
2026-06-19 01:49:16 +0530

    Fix url's

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO with CORS configuration
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"], // Frontend URL
+    origin: ["https://scope.mlrit.ac.in"], //modified
     methods: ["GET", "POST"],
     credentials: true,
   },
--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Backend API URL
+        target: "https://scope.mlrit.ac.in", // Backend API URL
         changeOrigin: true,
         secure: false,
       },
